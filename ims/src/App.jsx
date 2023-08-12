8f5a710031b433cabbd7114344674c99c06c07d9
Muhammad Ihtisham <riaz4u72nb+github@gmail.com>
2023-08-12 18:46:44 +0500

    first

---
+++ ims/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <h1 className='text-3xl font-sans text-black'>This is Tailwind + Vite + React</h1>
+    </div>
+  );
+}
+
+export default App
